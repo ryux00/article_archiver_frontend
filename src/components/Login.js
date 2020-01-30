d838c2650dc83cab81b6072521a119426cecacf5
nishit <nishitmohanan@gmail.com>
2020-01-31 00:08:54 +0530

    Added call to login api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,97 @@
 import React from "react";
-import { Button, Form, Icon, Input } from "antd";
+import { Button, Form, Icon, Input, Checkbox } from "antd";
 // const { Form, Icon, Input, Button, Checkbox } = antd;
 import "antd/dist/antd.css";
+class NormalLoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        const request_body = {
+          username: values.username,
+          password: values.password
+        };
+        fetch("http://127.0.0.1:8000/api/login/", {
+          method: "POST", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(request_body)
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log("Success:", data);
+          })
+          .catch(error => {
+            console.error("Error:", error);
+          });
+        console.log("Received values of form: ", request_body);
+      }
+    });
+  };
 
-function Login() {
-  return (
-    <div className="Login">
-      <header className="App-header">
-        <Form className="login-form">
-          <Input
-            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-            placeholder="Username"
-          />
-          <Input
-            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-            type="password"
-            placeholder="Password"
-          />
-          <Button type="primary">Login</Button>
-        </Form>
-      </header>
-    </div>
-  );
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div id="container">
+        <div id="form_container">
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <span className="login_header">Login</span>
+
+            <Form.Item>
+              {getFieldDecorator("username", {
+                rules: [
+                  { required: true, message: "Please input your username!" }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="Username"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator("password", {
+                rules: [
+                  { required: true, message: "Please input your Password!" }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  type="password"
+                  placeholder="Password"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator("remember", {
+                valuePropName: "checked",
+                initialValue: true
+              })(<Checkbox>Remember me</Checkbox>)}
+              <a className="login-form-forgot" href="/reset-password">
+                Forgot password
+              </a>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+              Or <a href="/register">register now!</a>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login;
+const WrappedNormalLoginForm = Form.create({ name: "normal_login" })(
+  NormalLoginForm
+);
+export default WrappedNormalLoginForm;
