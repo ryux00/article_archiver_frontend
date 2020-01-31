2ddea91d05cab9341d022963d8f2777e2878c8ff
nishit <nishitmohanan@gmail.com>
2020-02-01 01:40:43 +0530

    Added article list url to routing

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Login from "./Login";
+import ArticleList from "./ArticleList.js";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/article" component={ArticleList} />
     </Switch>
   </BrowserRouter>
 );
