3a9280a9453c8d75ccc13df90b920c51b86e1558
nishit <nishitmohanan@gmail.com>
2020-02-16 00:49:27 +0530

    Added registration component

---
+++ src/components/Register.js
@@ -0,0 +1,157 @@
+import React from "react";
+import {
+  Form,
+  Input,
+  Tooltip,
+  Icon,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete
+} from "antd";
+class Register extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+  compareToFirstPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("Password doesn't match");
+    } else {
+      callback();
+    }
+  };
+
+  validateToNextPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(["confirm"], { force: true });
+    }
+    callback();
+  };
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+      }
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0
+        },
+        sm: {
+          span: 16,
+          offset: 8
+        }
+      }
+    };
+    return (
+      <div id="container">
+        <div id="form_container">
+          <Form
+            {...formItemLayout}
+            onSubmit={this.handleSubmit}
+            className="registration-form"
+          >
+            <span class="login_header">Register</span>
+            <Form.Item
+              label={
+                <span>
+                  Username&nbsp;
+                  <Tooltip title="What do you want others to call you?">
+                    <Icon type="question-circle-o" />
+                  </Tooltip>
+                </span>
+              }
+            >
+              {getFieldDecorator("username", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                    whitespace: true
+                  }
+                ]
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="E-mail">
+              {getFieldDecorator("email", {
+                rules: [
+                  {
+                    type: "email",
+                    message: "The input is not valid E-mail!"
+                  },
+                  {
+                    required: true,
+                    message: "Please input your E-mail!"
+                  }
+                ]
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="Password" hasFeedback>
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please input your password!"
+                  },
+                  {
+                    validator: this.validateToNextPassword
+                  }
+                ]
+              })(<Input.Password />)}
+            </Form.Item>
+            <Form.Item label="Confirm Password" hasFeedback>
+              {getFieldDecorator("confirm", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please confirm your password!"
+                  },
+                  {
+                    validator: this.compareToFirstPassword
+                  }
+                ]
+              })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+            </Form.Item>
+
+            <Form.Item {...tailFormItemLayout}>
+              {getFieldDecorator("agreement", {
+                valuePropName: "checked"
+              })(
+                <Checkbox>
+                  I have read the <a href="">agreement</a>
+                </Checkbox>
+              )}
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit">
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedRegistrationForm = Form.create({ name: "register" })(Register);
+
+export default WrappedRegistrationForm;
--- src/components/Router.js
+++ src/components/Router.js
@@ -5,12 +5,14 @@ import Login from "./Login";
 import ArticleList from "./ArticleList.js";
 import ArticlePage from "./ArticlePage";
 import Logout from "./Logout";
+import Register from "./Register";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
       <Route exact path="/logout" component={Logout} />
       <Route exact path="/article" component={ArticleList} />
       <Route exact path="/article/:article" component={ArticlePage} />
